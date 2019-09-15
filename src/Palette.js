da0b2abb39299c37ae91885fe16759af143f6e24
naveen <naveentejas1109@gmail.com>
2019-09-15 17:01:07 +0530

    Started Colors app and added Palette component.

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* navbar here */}
+                {/* Color boxes */}
+                <div className="Palette-colors"> 
+                
+                </div> 
+                {/* Footer here */}
+            </div>
+        )
+    }
+}
